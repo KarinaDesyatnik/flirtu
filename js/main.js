9c58e291999a7823ce2943bcddfa8f0d139080e2
karinadesyatnik <karinadesyatnik27@gmail.com>
2019-11-12 23:00:56 +0300

    header and screen changes

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+
+$('.slider').slick({
+prevArrow: $('.prev'),
+nextArrow: $('.next'),
+autoplay: true,
+ fade: true,
+ autoplaySpeed: 7000,
+ cssEase: 'ease-in-out'
+
+});
+
+
+
+
+});
