9289c11fd33757c505ef12f40a95deaec16e8ce4
karinadesyatnik <karinadesyatnik27@gmail.com>
2021-04-25 01:19:50 +0300

    flirtu updates

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(function(){
+
+$('.cookis__setting').click(function(){
+	$(this).toggleClass('active');
+	$('.cookis__setting-content').toggleClass('active');
+})
+
+$('.cookis__setting-button').click(function(){
+	$('.cookis__setting-content').removeClass('active');
+	$('.cookis__setting').removeClass('active');
+})
+
+$('.cookis__setting-close').click(function(){
+	$('.cookis__setting').removeClass('active');
+	$('.cookis__setting-content').removeClass('active');
+})
+
+$('.accept__cookis').click(function(){
+	$('.cookis-modal').fadeOut();
+})
+
+$('.cooking-setting-footer').click(function(){
+	$('.cookis-setting-modal').fadeIn();
+})
+
+$('.cookis-setting-modal .cookis__setting-close').click(function(){
+	$('.cookis-setting-modal').fadeOut();
+})
+$('.cookis-setting-modal .cookis__setting-button').click(function(){
+	$('.cookis-setting-modal').fadeOut();
+})
+
+// ------start sticky header------
+
+$(window).scroll(function() {
+if ($(this).scrollTop() > 1){
+$('.top-header').addClass("sticky");
+}
+else{
+$('.top-header').removeClass("sticky");
+}
+});
+
+//-------end sticky header--------
+
+});
